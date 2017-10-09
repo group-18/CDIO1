b5b5aa53458bd0601bac2d49753bfcd688f03e28
Oliver <32456406+outrageousbog@users.noreply.github.com>
2017-10-09 18:02:25 +0200

    tilføjet metoder til player

--- src/spil/Game.java
+++ src/spil/Game.java
@@ -1,4 +1,16 @@
 package spil;
-
+import java.util.Scanner;
 public class Game {
+    public static void main (String [] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String a = scanner.nextLine();
+        String b = scanner.nextLine();
+
+        Player p1 = new Player(a);
+        Player p2 = new Player(b);
+
+        System.out.println(p1.getName());
+        System.out.println(p2.getName());
+    }
 }
--- src/spil/Player.java
+++ src/spil/Player.java
@@ -1,4 +1,27 @@
 package spil;
 
 public class Player {
+    private String navn;
+    private int score;
+
+    public Player(String navn){
+        this.navn=navn;
+    }
+
+    public String getName(){
+        return this.navn;
+    }
+
+    public int addScore(int faceValue) {
+        this.score = this.score + faceValue;
+    }
+
+    public int getScore(){
+        return this.score;
+    }
+
+    public int resetScore() {
+        this.score = 0;
+        return this.score;
+    }
 }
