7a387daa3986d41b69a0d82bbe618fb907fd1bd3
sebdtu <32480015+sebdtu@users.noreply.github.com>
2017-10-10 13:36:51 +0200

    Dice
    setFaceValue og getFaceValue. Ved ikke om vi skal bruge mere.

--- src/spil/Dice.java
+++ src/spil/Dice.java
@@ -1,4 +1,29 @@
 package spil;
 
 public class Dice {
+    private int faceValue;
+    private final int MAX=6;
+
+    public Die()
+    {
+        faceValue = 1;
+    }
+
+    //Math.random creates random integers from 1 - 6
+    public void roll()
+    {
+        faceValue = (int)(Math.random() * MAX) +1;
+    }
+
+    //Sets the face value
+    public void setFaceValue(int value)
+    {
+        faceValue=value;
+    }
+
+    //Gets the face value using the setFaceValue method
+    public int getFaceValue()
+    {
+        return faceValue;
+    }
 }
