6c33b9ab7b5dab9ba8054c977354b630a34453f2
Magnus Hansen <magnus.h1993@gmail.com>
2017-10-11 22:48:10 +0200

    Create test to test sum probability

--- test/spil/DiceTest.java
+++ test/spil/DiceTest.java
@@ -67,4 +67,52 @@ class DiceTest {
         assertTrue(rolls[5] < maxFailureRate, "Der er for mange 6'er " + rolls[5]);
     }
 
+    @Test
+    void rollCountMultiple() {
+        int numberOfDies = 2;
+        int numberOfRolls = 60000;
+
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+
+        int[] rolls = new int[numberOfDies * 6 + 1];
+
+        for (int i = 0; i < numberOfRolls; i++) {
+            d1.roll();
+            d2.roll();
+
+            int sum = d1.getFaceValue() + d2.getFaceValue();
+
+            rolls[sum]++;
+        }
+
+        double oneRollPercent = (double) 100 / numberOfRolls;
+
+        double totalPercentage = 0;
+        double failureRate = 0.75;
+        for (int i = numberOfDies; i < rolls.length; i++) {
+            double percentage = (double) rolls[i] * oneRollPercent;
+            double expectedPercentage = this.numPossibilities(numberOfDies, i) * (100.0 / 36.0);
+
+            assertTrue(percentage >= expectedPercentage - failureRate, "Sum " + i + " med sandsynlighed " + percentage + "% er for lav, min. " + (expectedPercentage - failureRate) + "%");
+            assertTrue(percentage <= expectedPercentage + failureRate, "Sum " + i + " med sandsynlighed " + percentage + "% er for høj, max. " + (expectedPercentage + failureRate) + "%");
+
+            totalPercentage += percentage;
+        }
+
+        assertTrue(totalPercentage >= 99 && totalPercentage <= 101, "Summen giver ikke 100% +- 1% : " + totalPercentage);
+    }
+
+
+    private int numPossibilities(int numDice, int sum) {
+        if (numDice == sum) {
+            return 1;
+        } else if (numDice == 0 || sum < numDice) {
+            return 0;
+        } else {
+            return this.numPossibilities(numDice, sum - 1) +
+                    this.numPossibilities(numDice - 1, sum - 1) -
+                    this.numPossibilities(numDice - 1, sum - 7);
+        }
+    }
 }
